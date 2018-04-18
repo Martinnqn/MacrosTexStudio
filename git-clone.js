58d404f1cbcf08eeb47d6939dd189d9a4156fce0
Martinnqn <bermudez_martin.nqn@hotmail.com>
2018-04-18 12:20:48 -0300

    clone ya no necesita tener un documento.tex abierto para clonar un repositorio

--- git-clone.js
+++ git-clone.js
@@ -9,10 +9,13 @@ if (repo!="" && dest!=""){
 str = "Está por clonar el repositorio "+repo+", ¿Seguro?";
 var res = confirm(str);
 if (res) {
-buildManager.runCommand('git clone \"'+repo+'\" \"'+dest+'\"', editor.fileName());
+app.fileNew();
+newEditor = documentManager.currentDocument.editorView.editor;
+buildManager.runCommand("git clone "+repo+" "+dest, newEditor.fileName());
 }   
 }else{
 str = "Debe completar todos los campos";
 var res = alert(str);
 }
 }
+
