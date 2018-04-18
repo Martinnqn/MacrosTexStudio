85a133ce56132f30c7abaf4f0cb164ef7011725f
Martinnqn <bermudez_martin.nqn@hotmail.com>
2018-04-18 11:38:38 -0300

    arreglado el clone

--- git-clone.js
+++ git-clone.js
@@ -3,16 +3,16 @@ var dialog = UniversalInputDialog("","Repository","repo");
 dialog.setWindowTitle("Git - Clone");
 dialog.add("","Destination folder","dest")
 if (dialog.exec()!=0){
-	var repo = dialog.get("repo");
-	var dest = dialog.get("dest");
-	if (repo!="" && dest!=""){
-		str = "Está por clonar el repositorio "+repo+", ¿Seguro?";
-		var res = confirm(str);
-		if (res) {
-			buildManager.runCommand("git clone "+repo+" "+dest, editor.fileName());
-		}   
-	}else{
-		str = "Debe completar todos los campos";
-		var res = alert(str);
-	}
+var repo = dialog.get("repo");
+var dest = dialog.get("dest");
+if (repo!="" && dest!=""){
+str = "Está por clonar el repositorio "+repo+", ¿Seguro?";
+var res = confirm(str);
+if (res) {
+buildManager.runCommand('git clone \"'+repo+'\" \"'+dest+'\"', editor.fileName());
+}   
+}else{
+str = "Debe completar todos los campos";
+var res = alert(str);
+}
 }
