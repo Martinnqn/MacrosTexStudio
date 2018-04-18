94efd468a5ffd09f93c30b2a37f6a49eb1c13bac
Martinnqn <bermudez_martin.nqn@hotmail.com>
2018-04-18 11:27:59 -0300

    add clone

---
+++ git-clone.js
@@ -0,0 +1,18 @@
+%SCRIPT
+var dialog = UniversalInputDialog("","Repository","repo");
+dialog.setWindowTitle("Git - Clone");
+dialog.add("","Destination folder","dest")
+if (dialog.exec()!=0){
+	var repo = dialog.get("repo");
+	var dest = dialog.get("dest");
+	if (repo!="" && dest!=""){
+		str = "Está por clonar el repositorio "+repo+", ¿Seguro?";
+		var res = confirm(str);
+		if (res) {
+			buildManager.runCommand("git clone "+repo+" "+dest, editor.fileName());
+		}   
+	}else{
+		str = "Debe completar todos los campos";
+		var res = alert(str);
+	}
+}
